d10593e68e9ed2179054770904433ff8388bdcab
Abhi Tyagi <abhi.tyagi994@gmail.com>
2026-08-29 01:41:33 +0100

    add experiment 3

--- notebooks/dagshub_setup.py
+++ notebooks/dagshub_setup.py
@@ -1,5 +1,11 @@
 
 import mlflow
 import dagshub
+dagshub.init(repo_owner='abhityagi994-prog', repo_name='mlops-mini-project', mlflow=True)
+
+mlflow.set_tracking_uri("https://dagshub.com/abhityagi994-prog/mlops-mini-project.mlflow")
 
 
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
